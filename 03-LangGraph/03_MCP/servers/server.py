1825e0cfab41bc439b93aead4c0a8132c270f33b
vijaynvb <vijay.nvb@gmail.com>
2026-03-13 13:35:04 +0530

    all other examples

---
+++ 03-LangGraph/03_MCP/servers/server.py
@@ -0,0 +1,21 @@
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("MCP Server")
+
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """Add two numbers"""
+    return a + b
+
+@mcp.tool()
+def multiply(a: int, b: int) -> int:
+    """Multiply two numbers"""
+    return a * b
+
+@mcp.tool()
+async def get_weather(location: str) -> str:
+    """Get weather for location."""
+    return "It's always sunny in New York"
+
+if __name__ == "__main__":
+    mcp.run(transport="streamable-http")
